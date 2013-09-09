dc7dfbd7501acd34469135449fc1680d83b34a0f
Zach Tellman <ztellman@gmail.com>
2013-09-08 21:39:36 -0700

    target latest proteus

--- project.clj
+++ project.clj
@@ -5,11 +5,14 @@
   :dependencies [[rhizome "0.1.9"]
                  [primitive-math "0.1.3"]
                  [potemkin "0.3.3"]
-                 [proteus "0.1.3"]
-                 [riddley "0.1.3-SNAPSHOT"]]
+                 [proteus "0.1.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [reiddraper/simple-check "0.2.1"]]}}
+                                  [reiddraper/simple-check "0.2.1"]
+                                  [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ^:replace ["-server"]
   :java-source-paths ["src"]
-  :javac-options ["-target" "1.5" "-source" "1.5"])
+  :javac-options ["-target" "1.5" "-source" "1.5"]
+  :plugins [[codox "0.6.4"]]
+  :codox {:writer codox-md.writer/write-docs
+          :include [automat.core]})
